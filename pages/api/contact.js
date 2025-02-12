dedf74073f3f04b82a234daf61f217648ceb49e0
divyesh350 <bakaraniyadivyesh94@gmail.com>
2025-02-12 21:20:31 +0530

    smtp

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,3 +1,4 @@
+import { sendEmail } from '@/lib/mail';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -13,15 +14,22 @@ export default async function handler(req, res) {
     if (!name || !email || !message) {
       return res.status(400).json({ error: 'All fields are required.' });
     }
+    const sender = {
+      name: name,
+      email: email,
+    }
+    const recipient = {
+      name: 'Divyesh',
+      email: 'listenmusicup@gmail.com',
+      }
+    const result = await sendEmail({
+      from: sender,
+      to: recipient,
+      subject: 'Contact Form Submission',
+      message: message
+    })
 
-    const data = await resend.emails.send({
-      from: 'onboarding@resend.dev',  // Change this to a verified domain if possible
-      to: process.env.RECEIVER_EMAIL, // Make sure this is set in .env.local
-      subject: `New Contact Form Submission from ${name}`,
-      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-    });
-
-    return res.status(200).json({ success: true, data });
+    return res.status(200).json({ success: true , result});
   } catch (error) {
     console.error('Error sending email:', error);
     return res.status(500).json({ error: 'Failed to send email' });
